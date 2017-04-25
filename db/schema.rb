291ff2cd2efd07cd220b1ef51bab8bab2c13123b
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-25 11:30:54 +0900

    Edited body test color on dashboard page.

--- db/schema.rb
+++ db/schema.rb
@@ -77,17 +77,6 @@ ActiveRecord::Schema.define(version: 20170424055943) do
     t.index ["user_id"], name: "index_notices_on_user_id", using: :btree
   end
 
-  create_table "notifications", force: :cascade do |t|
-    t.integer  "recepient_id"
-    t.integer  "actor_id"
-    t.datetime "read_at"
-    t.string   "action"
-    t.integer  "notifiable_id"
-    t.string   "notifiable_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.string   "state"
     t.integer  "inquiry_id"
