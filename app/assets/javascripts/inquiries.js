5a21d91fef8a629fcbe057efd62e0a25f249befd
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-27 10:12:35 +0900

    Changed position of question button and changed hover style.

--- app/assets/javascripts/inquiries.js
+++ app/assets/javascripts/inquiries.js
@@ -1,3 +1,9 @@
 $(function () {
-  $('[data-toggle="popover"]').popover()
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover',
+  })
 })
+
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip()
+// })
