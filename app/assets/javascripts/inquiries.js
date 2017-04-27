1c08449232cc1c1838f86809c1f39d89645fb4c9
Ainaa Azman <ainaasakinah@gmail.com>
2017-04-27 18:02:02 +0900

    Scroll to bottom of chat after refresh

--- app/assets/javascripts/inquiries.js
+++ app/assets/javascripts/inquiries.js
@@ -1,9 +1,15 @@
-$(function () {
+$(document).ready(function () {
   $('[data-toggle="popover"]').popover({
     trigger: 'hover',
-  })
-})
+  });
+
+  if ($(".chatbox-messages").length > 0) {
+    var height = $(".chatbox-messages")[0].scrollHeight;
+    $(".chatbox-messages").scrollTop(height);
+  }
+});
 
 // $(function () {
 //   $('[data-toggle="tooltip"]').tooltip()
 // })
+
